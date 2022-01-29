1ac2c926cdf300d4c46746d8330971786efa356d
mojahitsuji <satoshi@61.206.113.253.static.zoot.jp>
2022-01-29 23:57:04 +0100

    Finished Tutrial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,18 @@
 // Step 1: Import React
 import * as React from 'react'
 import Layout from '../components/Layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 // Step 2: Define your component
 const IndexPage = () => {
   return (
     <Layout pageTitle={"Home page"}>
       <p>I'm making this by following the Gatsby Tutrial.</p>
+      <StaticImage
+        alt='The man is reading the book with drinking coffee'
+        src='../images/photo-1518457900604-5c973dffdedf.jpeg'
+      />
+
     </Layout>
   )
 }
