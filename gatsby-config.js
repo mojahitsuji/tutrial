355f3937ee2c0f553d661ad3fc869d372de48302
mojahitsuji <satoshi@kawakaminoiMac.homenet.telecomitalia.it>
2022-02-03 19:42:55 +0100

    Finished Gatsby Tutorial Part 5

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,8 +10,9 @@ module.exports = {
         resolve: "gatsby-source-filesystem",
         options: {
           name: `blog`,
-          path: `${__dirname}/blog`,
+          path: `${__dirname}/blog/`,
         }
       },
+      "gatsby-plugin-mdx",
     ],
 }
